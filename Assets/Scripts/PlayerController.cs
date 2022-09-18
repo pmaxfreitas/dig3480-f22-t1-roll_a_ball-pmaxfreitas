96b7e7309185b311a420a0a7c1d926760196dddc
pmaxfreitas <108591661+pmaxfreitas@users.noreply.github.com>
2022-09-18 02:20:38 +0000

    Sep 17, 2022, 10:20 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -55,7 +55,7 @@ public class PlayerController : MonoBehaviour
             winText.text = "GameOver!";
             winTextObject.SetActive(true);
             speed = 0;
-            GameObject.Find("Player").GetComponent<Renderer>().material.color = new Color(188, 87, 43);
+            GameObject.Find("Player").GetComponent<Renderer>().material.color = new Color(14, 87, 109);
         }
     }
 
