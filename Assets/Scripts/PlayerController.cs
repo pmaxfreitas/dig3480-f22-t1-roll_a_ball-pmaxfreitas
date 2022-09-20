1532be31a65ce0fb3842f5a64987fb129a620557
pmaxfreitas <108591661+pmaxfreitas@users.noreply.github.com>
2022-09-20 01:36:20 +0000

    Sep 19, 2022, 9:36 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,7 +52,7 @@ public class PlayerController : MonoBehaviour
         }
         if (lives <= 0)
         {
-            winTextObject.GameObject.GetComponent<TextMeshPro>().text = "You lose!";
+            winTextObject.GetComponent<TextMeshPro>().text = "You lose!";
             winTextObject.SetActive(true);
             speed = 0;
             GameObject.Find("Player").GetComponent<Renderer>().material.color = new Color(0.05f, 0.38f, 0.43f);
