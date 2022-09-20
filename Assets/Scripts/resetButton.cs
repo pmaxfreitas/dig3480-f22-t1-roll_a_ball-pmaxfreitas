72b07d7b7822445185ec4ba1625d97d0cd67835a
pmaxfreitas <108591661+pmaxfreitas@users.noreply.github.com>
2022-09-20 02:12:22 +0000

    Sep 19, 2022, 10:12 PM

--- Assets/Scripts/resetButton.cs
+++ Assets/Scripts/resetButton.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class resetButton : MonoBehaviour
 {
-    public Button resetButton;
+    public Button reset;
 
     private void Reset()
     {
